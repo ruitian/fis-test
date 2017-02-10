cdaeef0791e0fd8a5f69885a3e7aeac7938b3555
ruitian <ike960705@gmail.com>
2017-02-10 14:02:24 +0800

    添加上传版本

---
+++ setup.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='tornado_mail',
+    author='ruitian',
+    description='',
+    url='https://github.com/ruitian/tornado_mail.git',
+    version='0.1.0',
+    install_requires=[
+        'tornado>=4.0.0'
+    ]
+)
